8e657ec5c498620979c013b25ae8e44223c2aa97
lorisfo <loris-fo@outlook.com>
2015-03-18 18:15:12 +0000

    Added angular project

---
+++ public/js/gitUserSearchController.js
@@ -0,0 +1,14 @@
+githubUserSearch.controller('GitUserSearchController', function($scope, $resource) {
+
+  var searchResource = $resource('https://api.github.com/search/users')
+
+  $scope.doSearch = function() {
+    $scope.searchResult = searchResource.get({
+      q: $scope.searchTerm
+    });
+  };
+
+  $scope.doClear = function() {
+    $scope.searchTerm = ""
+  };
+});
