b23dbf3c942f89130b73d5e02d0b2f886fe6e806
lorisfo <loris-fo@outlook.com>
2015-03-18 18:45:31 +0000

    deploying to heroku

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ var server = require('http').createServer(app);
 app.set('view engine', 'ejs');
 app.use('/public', express.static(__dirname + '/public'));
 
+var port = process.env.PORT || 3000;
+
 app.get('/', function(request, response) {
   response.render('index')
 });
 
 
-server.listen(3000, function() {
-  console.log("Server listenning on port 3000");
+server.listen(port, function() {
+  console.log("Server listenning on port " + port);
 });
 
 module.exports = server;
