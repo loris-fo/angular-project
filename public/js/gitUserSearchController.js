a081f92bcfa84dcd502670fd9002870acc05177b
lorisfo <loris-fo@outlook.com>
2015-03-19 16:47:32 +0000

    deployment working

--- public/js/gitUserSearchController.js
+++ public/js/gitUserSearchController.js
@@ -2,27 +2,25 @@ githubUserSearch.controller('GitUserSearchController', function($scope, $resourc
 
   var searchResource = $resource('https://api.github.com/search/users')
 
+  var repoResource = $resource('https://api.github.com/users/:user')
+
   $scope.doSearch = function (){
 
     $scope.searchResult = searchResource.get({
-      access_params: '587e4f0b8cbb52418e9230c1f3159e1a920138e6',
+      access_token: '587e4f0b8cbb52418e9230c1f3159e1a920138e6',
       q: $scope.searchTerm
     });
 
+    $scope.repoResult = repoResource.get({
+      access_token: '587e4f0b8cbb52418e9230c1f3159e1a920138e6',
+      user: $scope.searchTerm
+    })
   };
 
   $scope.doClear = function() {
     $scope.searchTerm = ""
   };
 
-  var name = 'https://api.github.com/users/' + $scope.searchTerm;
-      $http.get(name).success(function(data){
-          $scope.username = data
-        });
-    });
-
 });
-
-
 
 
