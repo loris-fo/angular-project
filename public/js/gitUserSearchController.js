cb290f931babf012d43f99f0fabb24b976b4045e
lorisfo <loris-fo@outlook.com>
2015-03-19 15:33:39 +0000

    deployment

--- public/js/gitUserSearchController.js
+++ public/js/gitUserSearchController.js
@@ -7,21 +7,22 @@ githubUserSearch.controller('GitUserSearchController', function($scope, $resourc
     $scope.searchResult = searchResource.get({
       access_params: '587e4f0b8cbb52418e9230c1f3159e1a920138e6',
       q: $scope.searchTerm
-    }, function(){
-      var name = 'https://api.github.com/users/' + $scope.searchTerm;
-      $http.get(name).
-        success(function(data){
-          $scope.username = data
-        });
     });
 
   };
+
+  $scope.doClear = function() {
+    $scope.searchTerm = ""
+  };
+
+  var name = 'https://api.github.com/users/' + $scope.searchTerm;
+      $http.get(name).success(function(data){
+          $scope.username = data
+        });
+    });
 
 });
 
 
 
 
-// $scope.doClear = function() {
-  //   $scope.searchTerm = ""
-  // };
