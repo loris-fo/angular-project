c48482b263d85da84fea62dbb126469e1dae88ef
lorisfo <loris-fo@outlook.com>
2015-03-19 13:30:06 +0000

    token access

--- public/js/gitUserSearchController.js
+++ public/js/gitUserSearchController.js
@@ -1,14 +1,27 @@
-githubUserSearch.controller('GitUserSearchController', function($scope, $resource) {
+githubUserSearch.controller('GitUserSearchController', function($scope, $resource, $http) {
 
   var searchResource = $resource('https://api.github.com/search/users')
 
-  $scope.doSearch = function() {
+  $scope.doSearch = function (){
+
     $scope.searchResult = searchResource.get({
+      access_params: '587e4f0b8cbb52418e9230c1f3159e1a920138e6',
       q: $scope.searchTerm
+    }, function(){
+      var name = 'https://api.github.com/users/' + $scope.searchTerm;
+      $http.get(name).
+        success(function(data){
+          $scope.username = data
+        });
     });
+
   };
 
-  $scope.doClear = function() {
-    $scope.searchTerm = ""
-  };
 });
+
+
+
+
+// $scope.doClear = function() {
+  //   $scope.searchTerm = ""
+  // };
